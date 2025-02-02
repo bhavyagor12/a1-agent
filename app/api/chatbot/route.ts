b12ad4dc60ffe4de8a99e5bb927d7c5aafa1bc79
Bhavya Gor <bhavya.gor9999@gmail.com>
2025-02-02 13:33:42 +0530

    fix build local

--- app/api/chatbot/route.ts
+++ app/api/chatbot/route.ts
@@ -5,32 +5,27 @@ import { MemorySaver } from "@langchain/langgraph";
 import { createReactAgent } from "@langchain/langgraph/prebuilt";
 import { HumanMessage } from "@langchain/core/messages";
 
-
 let conversationHistory: { [key: string]: any[] } = {};
 
+export async function POST(req: NextRequest) {
+  const { userId, userMessage } = await req.json();
 
-export async function POST(req: NextRequest, res: NextResponse) {
-    const { userId, userMessage } = await req.json();
+  if (!userId || !userMessage) {
+    return NextResponse.json({ error: "userId is required" }, { status: 400 });
+  }
 
-    if (!userId || !userMessage) {
-        return NextResponse.json(
-            { error: "userId is required" },
-            { status: 400 },
-          );
-    }
+  const llm = new ChatOpenAI({
+    model: "gpt-4o-mini",
+  });
 
-    const llm = new ChatOpenAI({
-        model: "gpt-4o-mini",
-    });
+  const memory = new MemorySaver();
+  const agentConfig = { configurable: { thread_id: userId } };
 
-    const memory = new MemorySaver();
-    const agentConfig = { configurable: { thread_id: userId } };
-
-    const agent = createReactAgent({
-      llm,
-      tools: [depositUSDCToAave],
-      checkpointSaver: memory,
-      messageModifier: `
+  const agent = createReactAgent({
+    llm,
+    tools: [depositUSDCToAave],
+    checkpointSaver: memory,
+    messageModifier: `
         You are a helpful DEFI agent that can interact onchain using the functions given to you. You are 
         empowered to interact onchain using your tools. If not, you can provide your wallet details and request 
         funds from the user. Before executing your first action, If someone askes to send USDC to AAVE. Ask them to send USDC to your wallet address. If they ask again then call the function accordingly. Make sure the user has entered how many USDC they want to send, if not ask again. 
@@ -38,35 +33,37 @@ export async function POST(req: NextRequest, res: NextResponse) {
         asks you to do something you can't do with your currently available tools, you must say so, Refrain from 
         restating your tools' descriptions unless it is explicitly requested. If a user explicitly asks to execute a tool do it without hesitating.
         `,
-    });
+  });
 
-    // Initialize memory for the user if not already created
-    if (!conversationHistory[userId]) {
-      conversationHistory[userId] = [];
-    }
-
-    // Add the user message to the conversation history
-   
-    conversationHistory[userId].push(new HumanMessage(userMessage));
-
-    if (!userMessage) {
-      return NextResponse.json(
-        { error: "Invalid request body" },
-        { status: 400 },
-      );
-    }
-
-    try {
-        const modelResponse = await agent.invoke({ messages: conversationHistory[userId] }, agentConfig);
-        conversationHistory[userId] = modelResponse.messages;
-        return NextResponse.json(conversationHistory[userId], {
-            status: 200,
-        });
-    } catch (error) {
-      return NextResponse.json(
-          { error: "Failed to get response from model" + error },
-          { status: 500 },
-      );
-    }
+  // Initialize memory for the user if not already created
+  if (!conversationHistory[userId]) {
+    conversationHistory[userId] = [];
   }
-  
+
+  // Add the user message to the conversation history
+
+  conversationHistory[userId].push(new HumanMessage(userMessage));
+
+  if (!userMessage) {
+    return NextResponse.json(
+      { error: "Invalid request body" },
+      { status: 400 },
+    );
+  }
+
+  try {
+    const modelResponse = await agent.invoke(
+      { messages: conversationHistory[userId] },
+      agentConfig,
+    );
+    conversationHistory[userId] = modelResponse.messages;
+    return NextResponse.json(conversationHistory[userId], {
+      status: 200,
+    });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to get response from model" + error },
+      { status: 500 },
+    );
+  }
+}
--- app/api/webhook/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-    console.log("Webhook request", req.body);
-    res.json({ message: "OK" });
- }
